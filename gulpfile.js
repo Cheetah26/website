f9fd4ba10c3bbb99cb628d5c5414a1d34069e50e
Shane <scmongo@gmail.com>
2020-06-03 00:10:02 -0400

    Add Minecraft section to Linux

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var data = require('gulp-data');
 
 var nunjucksRender = require('gulp-nunjucks-render');
-gulp.task('nunjucks', function(done) {
+gulp.task('nunjucks', function() {
 	gulp.src(['src/pages/**/*', '!src/pages/**/*.+(html|njk)']).pipe(gulp.dest('build'));
 	return gulp.src('src/pages/**/*.+(html|njk)')
 	    .pipe(data(function() {
@@ -12,37 +12,26 @@ gulp.task('nunjucks', function(done) {
 			path: ['src/templates']
 		}))
 		.pipe(gulp.dest('build'));
-	// done();
 });
 
 
-var prettify = require('gulp-html-prettify');
+// var prettify = require('gulp-html-prettify');
 var htmlmin = require('gulp-htmlmin');
 gulp.task('clean', function () {
-	gulp.src(['build/**/*', 'build/**/*.+!(html)']).pipe(gulp.dest('dist'));
+	gulp.src(['build/**/*', '!build/**/*.+(html)']).pipe(gulp.dest('dist'));
 	return gulp.src('build/**/*.html')
-		.pipe(prettify({indent_char: ' ', indent_size: 2}))
 		.pipe(htmlmin({ collapseWhitespace: true }))
 		.pipe(gulp.dest('dist'));
 });
 
 var imagemin = require('gulp-imagemin');
-// var fs = require('fs');
 gulp.task('imagemin', function () {
-	// var images = [];
 	return gulp.src( 'src/images/**/*.jpg' )
         .pipe(imagemin())
-		// .pipe(function (file) {
-		// 	images += {'name':file.basename, 'path':file.dirname};
-		// 	return file;
-		// })
         .pipe(gulp.dest('build/'));
-	// for (image in images) {
-	// 	fs.writeFile(image.path + 'images.json', , 'utf8', callback);
-	// }
 });
 
-var run = gulp.series('nunjucks');
+var run = gulp.series('nunjucks', 'clean');
 
 var watch = require('gulp-watch');
 gulp.task('watch', function () {
@@ -50,7 +39,6 @@ gulp.task('watch', function () {
 		return run();
 	});
 });
-
 
 gulp.task('default', function (done) {
 	return run();
