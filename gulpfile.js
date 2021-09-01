f241b6d546943da40996929f18c59d000467046f
Cheetah26 <scmongo@gmail.com>
2021-09-01 17:16:44 -0400

    First Iteration

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+
+var pug = require('gulp-pug');
+gulp.task('build_html', function() {
+  return gulp.src('./src/pages/**/*.pug')
+    .pipe(pug({
+      extension: 'html',
+      basedir: `${__dirname}/public`
+    }))
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('build_css', function() {
+  return gulp.src('./src/styles/**/*.css')
+    .pipe(gulp.dest('./dist/styles'))
+})
+
+gulp.task('copy_files', function() {
+  return gulp.src('./src/files/**/*.*')
+    .pipe(gulp.dest('./dist/files'))
+})
+
+gulp.task('build', gulp.series('build_html', 'build_css', 'copy_files'));
+
+var browserSync = require('browser-sync').create();
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist',
+    },
+  });
+  browserSync.watch('./src/**/*.*', {}, gulp.series('build', browserSync.reload));
+})
+
+
+gulp.task('default', gulp.series('serve'));
